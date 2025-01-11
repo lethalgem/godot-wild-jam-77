767a5f0c3e90ed101f9f7c5db98a656739ffe5e8
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-01-11 15:36:59 -0600

    Scale udpates

--- godot-wild-jam-77/Scripts/scale.gd
+++ godot-wild-jam-77/Scripts/scale.gd
@@ -4,9 +4,9 @@ class_name Scale extends StaticBody2D
 @onready var bowl_collision_polygon_2d = $BowlCollisionPolygon2D
 @onready var area_2d = $InsideArea2D
 var total_weight : float = 0
-var goal_weight : float = 25
-var weights = {}
-
+var goal_weight : float = 100
+var type_count = {}
+var type_weights = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,34 +14,36 @@ func _ready():
 	bowl_collision_polygon_2d.position = polygon_2d.position
 	bowl_collision_polygon_2d.scale = polygon_2d.scale
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta:float) -> void:
 	pass
 
-func calculate_weight(weights):
-	for i in weights:
-		total_weight += i
-	if total_weight == goal_weight:
-		print("Winner")
+func calculate_weight(type_count):
+	if type_count:
+		for type in type_count.keys():
+			total_weight = type_count[type] * type_weights[type]
+			print(total_weight)
+			if total_weight >= goal_weight:
+				print("Winner")
 
 func _on_inside_area_2d_body_entered(body: Node2D):
 	if body is OrbBody:
 		var orb = body.get_orb()
-		if weights.has(orb.type):
-			var count = weights[orb.type]
+		if type_count.has(orb.type):
+			var count = type_count[orb.type]
 			count += 1
-			weights[orb.type] = count
-			print(weights)
+			type_count[orb.type] = count
+			calculate_weight(type_count)
 		else:
-			weights[orb.type] = 1
-			print(weights)
-			
+			type_count[orb.type] = 1
+			type_weights[orb.type] = orb.weight
+			calculate_weight(type_count)
+
 func _on_inside_area_2d_body_exited(body: Node2D):
 	if body is OrbBody:
 		var orb = body.get_orb()
-		if weights.has(orb.type):
-			var count = weights[orb.type]
+		if type_count.has(orb.type):
+			var count = type_count[orb.type]
 			count -= 1
-			weights[orb.type] = count
-			print(weights)
+			type_count[orb.type] = count
+			calculate_weight(type_count)
