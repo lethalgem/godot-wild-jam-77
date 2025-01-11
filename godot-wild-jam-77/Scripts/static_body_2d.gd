6583a47cf455ce9d7b42b6fb054dc8f6212b8b02
ChapoTheGoat <gama_giovanni@yahoo.com>
2025-01-10 21:25:41 -0600

    Gios Additions

--- godot-wild-jam-77/Scripts/static_body_2d.gd
+++ godot-wild-jam-77/Scripts/static_body_2d.gd
@@ -1,14 +1,21 @@
 extends StaticBody2D
 
 @onready var polygon_2d = $Polygon2D
-@onready var collision_polygon_2d = $CollisionPolygon2D
-
+@onready var bowl_collision_polygon_2d = $BowlCollisionPolygon2D
+var total_weight = 0
+var weights = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	collision_polygon_2d.polygon = polygon_2d.polygon
-	collision_polygon_2d.position = polygon_2d.position
+	bowl_collision_polygon_2d.polygon = polygon_2d.polygon
+	bowl_collision_polygon_2d.position = polygon_2d.position
+	bowl_collision_polygon_2d.scale = polygon_2d.scale
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func calcualte_weight_on_body_enter(body):
+	total_weight += weights
+	pass
+	
