30f683d2d427bf2a246afb4e80824351e11f001c
lethalgem <iwc123@gmail.com>
2025-01-12 10:31:35 -0500

    Prevent duplicating combos

--- godot-wild-jam-77/Scripts/orb_manager.gd
+++ godot-wild-jam-77/Scripts/orb_manager.gd
@@ -8,7 +8,8 @@ class_name OrbManager extends Node2D
 
 var orb_type_queue: Array[OrbType] = [OrbType.Water.new(), OrbType.Water.new(), OrbType.Fire.new()]
 var total_spawned: int = 0
-var spawned_orbs = {} # {id, orb} for easy lookup 
+var spawned_orbs = {} # {id, orb} for easy lookup
+var in_progress_combos = {} # {id, true} to prevent duplicating combos with easy comparison
 
 func add_new_orb_to_queue() -> void:
 	var possible_types = [OrbType.Fire, OrbType.Water]
@@ -28,8 +29,17 @@ func add_spawned(orb: Orb) -> void:
 	spawned_orbs[str(orb.id)] = orb
 	orb.combo_made_with.connect(handle_combo)
 
-# TODO: Animation showing the orbs merge
+# TODO: Animation showing the orbs merge -- JUICE it up, screen shake, particle effects, big numbers!
 func handle_combo(ids: Array[String], result: OrbType):
+	print("test")
+	
+	for id in ids:
+		if in_progress_combos.has(id):
+			return
+		else:
+			in_progress_combos[id] = true
+	
+	print(in_progress_combos)
 	var merge_position: Vector2
 	for index in range(ids.size()):
 		var combo_orb: Orb = spawned_orbs[ids[index]]
@@ -41,6 +51,12 @@ func handle_combo(ids: Array[String], result: OrbType):
 				debug_marker.global_position = merge_position
 		
 		combo_orb.queue_free()
-		spawned_orbs.erase([ids[index]])
+		spawned_orbs.erase(combo_orb.id)
+	
+	# Cleanup the in_progress_combos id list after arbitrary period (combo should be done by then)
+	get_tree().create_timer(1).timeout.connect((
+		func(ids):
+			for id in ids:
+				in_progress_combos.erase(id)).bind(ids))
 		
 	orb_spawner.spawn_orb_at(merge_position, result)
